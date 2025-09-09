022308bd00704c231e1a483eef19f12bcfc602cf
roopeshmrkesumr <manasmr62@gmail.com>
2025-09-09 09:40:59 +0530

    heading1

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <div>
+        <h1>i have created a new branch</h1>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
